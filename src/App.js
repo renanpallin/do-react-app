a947fa6411dc233e20521763ecd96843256f4dc3
Renan Pallin <renanpallin@gmail.com>
2021-11-19 21:18:50 -0300

    Pronto para o deploy

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Button } from '@material-ui/core';
 
-const api = process.env.REACT_APP_API_URL;
-
 function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    async function getMessage() {
-      try {
-        const { data } = await axios.get(api);
-        setMessage(data.message);
-      } catch (error) {
-        console.error('Algo deu errado na API');
-        setMessage('deu ruim =(');
-      }
-    }
-    getMessage();
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {message}
         <Button
           style={{
             marginTop: '20px',
