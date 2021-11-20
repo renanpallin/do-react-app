4144baf610079156c1805bcb8cc28237dc1e4779
Renan Pallin <renanpallin@gmail.com>
2021-11-19 21:51:15 -0300

    Buscando mensagem da API

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import './App.css';
 
 import { Button } from '@material-ui/core';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    async function getMessage() {
+      try {
+        const { data } = await axios.get(apiUrl);
+        setMessage(data.message);
+      } catch (error) {
+        console.error('Algo deu errado na api', error);
+        setMessage('deu ruim =(');
+      }
+    }
+
+    getMessage();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {message}
         <Button
           style={{
             marginTop: '20px',
